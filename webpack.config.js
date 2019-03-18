266987408693c378de04ee8d5ca7c1e53f674721
unknown <world0100@yandex.ru>
2019-03-18 23:13:01 +0300

    Task 2

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
